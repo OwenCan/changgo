e8ef8d6ac3dd0a92b92a60b5dff7cacc8472bd30
OwenZhao <760141714@qq.com>
2020-10-29 15:16:02 +0800

    添加Canal监听器

---
+++ changgou-parent/changgou-service/changgou-service-canal/src/main/java/com/changgou/CanalApplication.java
@@ -0,0 +1,23 @@
+package com.changgou;
+
+import com.xpand.starter.canal.annotation.EnableCanalClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @program: changgou
+ * @description: Canal微服务启动类
+ * @author: Owen Zhao
+ * @create: 2020-10-29 11:02
+ */
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableEurekaClient
+@EnableCanalClient
+public class CanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class, args);
+    }
+}
