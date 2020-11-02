87ffec4ebbf85255d4f9a08babd01828e5ccef28
OwenZhao <760141714@qq.com>
2020-11-02 18:23:31 +0800

    增加搜索信息

---
+++ changgou-parent/changgou-service/changgou-service-search/src/main/java/com/changgou/search/service/impl/SearchResultMapperImpl.java
@@ -0,0 +1,10 @@
+package com.changgou.search.service.impl;
+
+/**
+ * @program: changgou
+ * @description:
+ * @author: Owen Zhao
+ * @create: 2020-11-02 17:07
+ */
+public class SearchResultMapperImpl {
+}
--- changgou-parent/changgou-service/changgou-service-search/src/main/java/com/changgou/search/service/impl/SkuServiceImpl.java
+++ changgou-parent/changgou-service/changgou-service-search/src/main/java/com/changgou/search/service/impl/SkuServiceImpl.java
@@ -2,15 +2,22 @@ package com.changgou.search.service.impl;
 
 import com.alibaba.fastjson.JSON;
 import com.changgou.goods.feign.SkuFeign;
+import com.changgou.goods.pojo.Brand;
 import com.changgou.goods.pojo.Sku;
 import com.changgou.search.dao.SkuEsMapper;
 import com.changgou.search.service.SkuService;
 import com.search.SkuInfo;
 import entity.Result;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.terms.StringTerms;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
@@ -18,13 +25,11 @@ import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilde
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @program: changgou
+ * ctrl+alt+M 快捷键封装方法
  * @description: Sku
  * @author: Owen Zhao
  * @create: 2020-10-30 11:09
@@ -65,31 +70,149 @@ public class SkuServiceImpl implements SkuService {
             keyWord = "华为";
         }
         //2、创建查询对象的构建对象
-        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+        NativeSearchQueryBuilder nativeSearchQueryBuilder = new NativeSearchQueryBuilder();
 
-        //3、创建查询条件
+        //3、创建查询条件 size表示每次查出多少条，默认是10条
         //设置分组条件  商品分类
-        queryBuilder.addAggregation(AggregationBuilders.terms("skuCategorygroup").field("categoryName").size(50));
-        queryBuilder.withQuery(QueryBuilders.matchQuery("name", keyWord));
+        nativeSearchQueryBuilder.addAggregation(AggregationBuilders.terms("skuCategorygroup").field("categoryName").size(50));
+        //设置分组条件  商品品牌
+        nativeSearchQueryBuilder.addAggregation(AggregationBuilders.terms("skuBrandgroup").field("brandName").size(50));
+        //设置分组条件  商品规格 keyword表示不要进行分词
+        nativeSearchQueryBuilder.addAggregation(AggregationBuilders.terms("skuSpecgroup").field("spec.keyword").size(100));
 
+        //设置主关键字查询
+        nativeSearchQueryBuilder.withQuery(QueryBuilders.matchQuery("name", keyWord));
+
+        //使用filter先过滤商品和品牌 组合条件过滤
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        if (!StringUtils.isEmpty(searchMap.get("brand"))) {
+            boolQueryBuilder.filter(QueryBuilders.termQuery("brandName", searchMap.get("brand")));
+        }
+        if (!StringUtils.isEmpty(searchMap.get("category"))) {
+            boolQueryBuilder.filter(QueryBuilders.termQuery("categoryName", searchMap.get("category")));
+        }
+        //规格过滤查询
+        if (searchMap != null) {
+            for (String key : searchMap.keySet()) {
+                if (key.startsWith("spec_")) {
+                    boolQueryBuilder.filter(QueryBuilders.termQuery("specMap." + key.substring(5) + ".keyword", searchMap.get(key)));
+                }
+            }
+        }
+        //价格过滤查询
+        String price = searchMap.get("price");
+        if (!StringUtils.isEmpty(price)) {
+            String[] split = price.split("-");
+            if (!split[1].equalsIgnoreCase("*")) {
+                boolQueryBuilder.filter(QueryBuilders.rangeQuery("price").from(split[0], true).to(split[1], true));
+            } else {
+                boolQueryBuilder.filter(QueryBuilders.rangeQuery("price").gte(split[0]));
+            }
+        }
+        //构建过滤查询
+        nativeSearchQueryBuilder.withFilter(boolQueryBuilder);
+
+        //构建排序查询
+        String sortRule = searchMap.get("sortRule");
+        String sortField = searchMap.get("sortField");
+        if (!StringUtils.isEmpty(sortRule) && !StringUtils.isEmpty(sortField)) {
+            nativeSearchQueryBuilder.withSort(SortBuilders.fieldSort(sortField).order(sortRule.equals("DESC") ? SortOrder.DESC : SortOrder.ASC));
+        }
+
+        //设置高亮条件
+        nativeSearchQueryBuilder.withHighlightFields(new HighlightBuilder.Field("name"));
+        nativeSearchQueryBuilder.withHighlightBuilder(new HighlightBuilder().preTags("<em style=\"color:red\">").postTags("</em>"));
+        //设置主关键字查询
+//        nativeSearchQueryBuilder.withQuery(QueryBuilders.multiMatchQuery(keywords,"name","brandName","categoryName"));
+
+
+        //构建分页查询
+        Integer pageNum = 1;
+        if (StringUtils.isEmpty(searchMap.get("pageNum"))) {
+            try {
+                pageNum = Integer.valueOf(searchMap.get("pageNum"));
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                pageNum = 1;
+            }
+        }
+        Integer pageSize = 3;
+        nativeSearchQueryBuilder.withPageable(PageRequest.of(pageNum, pageSize));
 
         //4、构造查询对象
-        NativeSearchQuery query = queryBuilder.build();
+        NativeSearchQuery query = nativeSearchQueryBuilder.build();
+//        AggregatedPage<SkuInfo> skuPage = esTemplate.queryForPage(query, SkuInfo.class, new SearchResultMapperImpl());
+
+
         //5、执行查询
         AggregatedPage<SkuInfo> skuPage = esTemplate.queryForPage(query, SkuInfo.class);
 
-        //获取分组结果
-        StringTerms stringTerms = (StringTerms) skuPage.getAggregation("skuCategorygroup");
-        List<String> categoryList = getStringsCategoryList(stringTerms);
+        //获取分组结果 商品分类
+        StringTerms stringTermsCategroy = (StringTerms) skuPage.getAggregation("skuCategorygroup");
+        //获取分组结果 商品品牌
+        StringTerms stringTermsBrand = (StringTerms) skuPage.getAggregation("skuBrandgroup");
+        //获取分组结果 商品规格
+        StringTerms stringTermsSpec = (StringTerms) skuPage.getAggregation("skuSpecgroup");
+
+        List<String> categoryList = getStringsCategoryList(stringTermsCategroy);
+        List<String> brandList = getStringBrandList(stringTermsBrand);
+        Map<String, Set<String>> specMap = getStringSpecMap(stringTermsSpec);
 
         //返回结果
         Map resultMap = new HashMap();
         resultMap.put("categoryList", categoryList);
+        resultMap.put("brandList", brandList);
+        resultMap.put("specMap", specMap);
         resultMap.put("rows", skuPage.getContent());
         resultMap.put("total", skuPage.getTotalElements());
         resultMap.put("totalPage", skuPage.getTotalPages());
 
         return resultMap;
+    }
+
+    /**
+     * 获取商品规格 存储set避免重复
+     * @param stringTermsSpec
+     * @return
+     */
+    private Map<String, Set<String>> getStringSpecMap(StringTerms stringTermsSpec) {
+        Map<String, Set<String>> specMap = new HashMap<>();
+        Set<String> specList = new HashSet<>();
+        if (stringTermsSpec != null) {
+            for (StringTerms.Bucket bucket : stringTermsSpec.getBuckets()) {
+                specList.add(bucket.getKeyAsString());
+            }
+        }
+        for (String specjson : specList) {
+            Map<String, String> map = JSON.parseObject(specjson, Map.class);
+            for (Map.Entry<String, String> entry : map.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                //获取之前存储的key值，避免新产生的规格数被覆盖
+                Set<String> specValues = specMap.get(key);
+                if (specValues == null) {
+                    specValues = new HashSet<>();
+                }
+                specValues.add(value);
+                specMap.put(key, specValues);
+            }
+        }
+        return specMap;
+    }
+
+    /**
+     *
+     * @param stringTermsBrand
+     * @return
+     */
+    private List<String> getStringBrandList(StringTerms stringTermsBrand) {
+        List<String> brandList = new ArrayList<>();
+        if (stringTermsBrand != null) {
+            for (StringTerms.Bucket bucket : stringTermsBrand.getBuckets()) {
+                brandList.add(bucket.getKeyAsString());
+            }
+        }
+        return brandList;
     }
 
     /**
